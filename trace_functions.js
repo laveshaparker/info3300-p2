e9ce60afc4b90d2c23aa79a4aa97c8b0a4d95142
La Vesha Parker <ldp47@cornell.edu>
2015-04-24 14:26:20 -0400

    Added functionality so we can dynamically render Twitter widgets. Will be useful when I finally figure out svg element click events....

--- trace_functions.js
+++ trace_functions.js
@@ -1,77 +1,80 @@
-		var show1Orbit = true;
-		//plot current ISS position
-		var plotCircle = function(){
-			$(".curCircle").remove();
-			$("#iss").remove();
-			svg.append("circle") //test circle for selecting point
-			   .attr("cx", projection(current.coords)[0]) //x-longitude
-			   .attr("cy", projection(current.coords)[1]) //y-latitude
-			   .attr("r", 2) 
-			   .attr("class", "curCircle")
-			   .attr("style", "stroke: blue; stroke-width: 8; fill: #000000");
-			svg.append("svg:image")
-				.attr("x", projection(current.coords)[0])
-				.attr("y", projection(current.coords)[1])
-				.attr('width', 65)
-				.attr('height', 35)
-				.attr("id", "iss")
-				.attr('xlink:href', "images/iss.gif");
-			};
-		function moveCircle() {
-			svg.select('.curCircle')
-				.attr("cx", projection(current.coords)[0])
-				.attr("cy", projection(current.coords)[1]);
-			svg.select('#iss')
-				.attr("x", projection(current.coords)[0])
-				.attr("y", projection(current.coords)[1]);
-		}
-		//plot current ISS orbit trace for current day
-		var plotDayTrace = function(){
-			$(".dayCircle").remove();
-			svg.selectAll('circle')
-				.data(orbitArr[$('#days').val()])
-				.enter()
-				.append("circle")
-				.attr("r", 2)
-				.attr("class", "dayCircle")
-				.attr("transform", function(d) { 
-					return "translate(" + projection(d.coords) + ")";
-				})
-				.attr("data-coords", function(d){
-					return d.coords;
-				})
-				.style("fill", 'red');
-			plotCircle();
-		};
-		//plot current ISS orbit trace for current orbit
-		var plotCurOrbit = function(){
-			$(".curOrbitCircle").remove();
-			svg.selectAll('circle')
-				.data(orbitData[current.id].orbit.coordinates)
-				.enter()
-				.append("circle")
-				.attr("r", 2)
-				.attr("class", "curOrbitCircle")
-				.attr("transform", function(d) { 
-					return "translate(" + projection(d) + ")";
-				})
-				.attr("data-coords", function(d){
-					return d;
-				})
-				.style("fill", 'red');
-			plotCircle();
-		};
-		function showDay() {
-			show1Orbit = false;
-			$('#showDay').hide();
-			$('#showOrbits').show();
-			$(".curOrbitCircle").remove();
-			plotDayTrace();
-		}
-		function showOrbit() {
-			show1Orbit = true;
-			$('#showOrbits').hide();
-			$('#showDay').show();
-			$(".dayCircle").remove();
-			plotCurOrbit();
-		}
+        var show1Orbit = true;
+        //plot current ISS position
+        var plotCircle = function(){
+            $(".curCircle").remove();
+            $("#iss").remove();
+            svg.append("circle") //test circle for selecting point
+               .attr("cx", projection(current.coords)[0]) //x-longitude
+               .attr("cy", projection(current.coords)[1]) //y-latitude
+               .attr("r", 2) 
+               .attr("class", "curCircle")
+               .attr("style", "stroke: blue; stroke-width: 8; fill: #000000");
+            svg.append("svg:image")
+                .attr("x", projection(current.coords)[0])
+                .attr("y", projection(current.coords)[1])
+                .attr('width', 65)
+                .attr('height', 35)
+                .attr("id", "iss")
+                .attr('xlink:href', "images/iss.gif");
+            $("#iss").on("click", function() {console.log("A tweet was clicked.")});
+            
+            };
+        function moveCircle() {
+            svg.select('.curCircle')
+                .attr("cx", projection(current.coords)[0])
+                .attr("cy", projection(current.coords)[1]);
+            svg.select('#iss')
+                .attr("x", projection(current.coords)[0])
+                .attr("y", projection(current.coords)[1]);
+
+        }
+        //plot current ISS orbit trace for current day
+        var plotDayTrace = function(){
+            $(".dayCircle").remove();
+            svg.selectAll('circle')
+                .data(orbitArr[$('#days').val()])
+                .enter()
+                .append("circle")
+                .attr("r", 2)
+                .attr("class", "dayCircle")
+                .attr("transform", function(d) { 
+                    return "translate(" + projection(d.coords) + ")";
+                })
+                .attr("data-coords", function(d){
+                    return d.coords;
+                })
+                .style("fill", 'red');
+            plotCircle();
+        };
+        //plot current ISS orbit trace for current orbit
+        var plotCurOrbit = function(){
+            $(".curOrbitCircle").remove();
+            svg.selectAll('circle')
+                .data(orbitData[current.id].orbit.coordinates)
+                .enter()
+                .append("circle")
+                .attr("r", 2)
+                .attr("class", "curOrbitCircle")
+                .attr("transform", function(d) { 
+                    return "translate(" + projection(d) + ")";
+                })
+                .attr("data-coords", function(d){
+                    return d;
+                })
+                .style("fill", 'red');
+            plotCircle();
+        };
+        function showDay() {
+            show1Orbit = false;
+            $('#showDay').hide();
+            $('#showOrbits').show();
+            $(".curOrbitCircle").remove();
+            plotDayTrace();
+        }
+        function showOrbit() {
+            show1Orbit = true;
+            $('#showOrbits').hide();
+            $('#showDay').show();
+            $(".dayCircle").remove();
+            plotCurOrbit();
+        }
--- tweets.js
+++ tweets.js
@@ -6,22 +6,30 @@ function checkForEntry(tweet, active) {
     });
     return false;
 }
-
+ 
 function TweetHandler(tweets) {
+    function setTweets() {
+        returnArr = [];
+        tweets.forEach(function(tweet) {
+            returnArr.push(Tweet(tweet));
+        });
+        return returnArr;
+    }
     return {
-        tweets : tweets,
-
+        tweets : setTweets(),
+ 
         // Tweet[], The tweets currently on the map
         active : [],
-
+ 
         // Retrieves the tweets from midnight on a given day to the 
         // current timestamp of the slider
-        serveTweets : function(current, midnight_day_of) {
+        serveTweets : function(current) {
             // Serves the last tweet that was before or at a given timestamp.
             // Called on change of var current.
             active = this.active;
             this.tweets.forEach(function(tweet) {
-                if (midnight_day_of <= tweet.timestamp && tweet.timestamp <= Date.parse(current.date)) {
+
+                if (0 <= tweet.timestamp && tweet.timestamp <= Date.parse(current.date)) {
                     if (!checkForEntry(tweet, active)) {
                         t = Tweet(tweet);
                         this.active.push(t);
@@ -29,16 +37,18 @@ function TweetHandler(tweets) {
                 }
             });
         },
-
+ 
         // Plot all of the "active" tweets
         plotTweets : function() {
+            // $('.tweet').remove();
             this.active.forEach(function(tweet) {
                 tweet.plotTweet(svg);
             });
+            // Inefficient, I know, but it works. I'll look into a better solution later.
         },
     }
 }
-
+ 
 function Tweet(tweet, map) {
     return {
         id          : tweet.id,
@@ -46,13 +56,26 @@ function Tweet(tweet, map) {
         html        : tweet.html,
         coordinates : current.coords,
         plotTweet   : function(map) {
-            map.append("circle") //test circle for selecting point
-            // TODO: Gotta fix
-                   .attr("cx", projection(this.coordinates)[0]) //x-longitude
-                   .attr("cy", projection(this.coordinates)[1]) //y-latitude
-                   .attr("r", 4)
-                   .attr("class", "curCircle")
-                   .attr("style", "stroke: red; stroke-width: 8; fill: #000000");
+            console.log('here2');
+            map.append("svg:circle") //test circle for selecting point
+               .attr("cx", projection(current.coords)[0]) //x-longitude
+               .attr("cy", projection(current.coords)[1]) //y-latitude
+               .attr("r", 4) 
+               .attr("class", "tweetCircle")
+               .attr("id", this.id)
+               .attr("style", "stroke: #4099FF; stroke-width: 8; fill: #4099FF");
+            console.log($("#" + this.id));
+            $("#" + this.id).on("click", function() {console.log("THIS tweet was clicked"); this.showTweet()});
         },
+
+        showTweet : function() {
+            document.getElementById("displayTweet").innerHTML = this.html;
+            twttr.widgets.load();
+        },
+
+        hideTweet : function() {
+            document.getElementById("displayTweet").innerHTML = "";
+        }
+ 
     }
 }
